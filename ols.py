ca7038efb0b6955f3da81bae6ae1644efd644acd
Gabriel <z5362310@ad.unsw.edu.au>
2025-04-15 13:38:05 +1000

    Simple refactor

--- ols.py
+++ ols.py
@@ -1,34 +1,33 @@
 import numpy as np
 
 class OLS:
-  def __init__(self, intercept=True):
-    self.intercept = intercept
+  def __init__(self, fit_intercept=True):
+    self.fit_intercept = fit_intercept
+    self.intercept = 0
 
   # Fit an OLS model of paramaters A onto vector b
   def fit(self, A, b):
-    self.A = A
-    self.b = b
-
-    if self.intercept:
+    if self.fit_intercept:
       A = np.column_stack((np.ones((len(A), 1)), A))
 
     # Find the least-squares solution to the linear system Ax = b
-    self.x = np.linalg.lstsq(A, b, rcond=None)[0]
-
+    x = np.linalg.lstsq(A, b, rcond=None)[0]
+    
+    # Store the weights and intercept
+    if self.fit_intercept:
+      self.x = x[1:]
+      self.intercept = x[0]
+    else:
+      self.x = x
+  
   # Predict the value of b given a set of parameters A
   def predict(self, A):
-    return A @ self.x
+    return A @ self.x + self.intercept
 
   # Get the weights of the model x
   def get_weights(self):
-    if self.intercept:
-      return self.x[1:]
-    else:
-      return self.x
+    return self.x
   
   # Get the constant term of the model
   def get_intercept(self):
-    if self.intercept:
-      return self.x[0]
-    else:
-      return 0
+   return self.intercept
